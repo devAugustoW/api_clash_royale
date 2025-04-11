eacb7bfbd26a167d0a59df45ae82a3ad1bd6ac05
Augusto Dantas <augustodantas.dev@gmail.com>
2025-04-11 09:33:54 -0300

    Estruturando a arquitetura de pastas do projeto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 
+import battleRoutes from './routes/battleRoutes';
+import cardRoutes from './routes/cardRoutes';
+import playerRoutes from './routes/playerRoutes';
 
 const app = express();
 
 app.use(express.json());
 
-
+//app.use('/api/battles', battleRoutes);
+//app.use('/api/cards', cardRoutes);
+//app.use('/api/players', playerRoutes);
 
 app.get('/', (req, res) => {
   res.send('API Clash Royale funcionando!');
